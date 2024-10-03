18ea246d2c3c010d1047023d1f3f84e39e9d7758
zhiftyDK <juulstausholmoscar@gmail.com>
2024-10-03 04:41:54 +0200

    added stream geolocator

---
+++ stream_geolocator.py
@@ -0,0 +1,65 @@
+# pip3 install maxminddb-geolite2
+
+from geolite2 import geolite2
+import socket, subprocess 
+
+cmd = r"tshark"
+
+# if ethernet try
+# cmd = r"C:\Program Files\Wireshark\tshark.exe -i ethernet"
+
+# you can list all your interfaces by running "tshark.exe --list-interfaces"
+# then if for instance you want to use the 4th try:
+# cmd = r"C:\Program Files\Wireshark\tshark.exe -i 4"
+
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+my_ip = socket.gethostbyname(socket.gethostname())
+reader = geolite2.reader()
+
+def get_ip_location(ip):
+    location = reader.get(ip)
+    
+    try:
+        country = location["country"]["names"]["en"]
+    except:
+        country = "Unknown"
+
+    try:
+        subdivision = location["subdivisions"][0]["names"]["en"]
+    except:
+        subdivision = "Unknown"    
+
+    try:
+        city = location["city"]["names"]["en"]
+    except:
+        city = "Unknown"
+    
+    return country, subdivision, city
+
+
+for line in iter(process.stdout.readline, b""):
+    columns = str(line).split(" ")
+
+    if "SKYPE" in columns or "UDP" in columns:
+        
+        # for different tshark versions
+        if "->" in columns:
+            src_ip = columns[columns.index("->") - 1]
+        elif "\\xe2\\x86\\x92" in columns:
+            src_ip = columns[columns.index("\\xe2\\x86\\x92") - 1]
+        else:
+            continue
+            
+        if src_ip == my_ip:
+            continue
+
+        try:
+            country, sub, city = get_ip_location(src_ip)
+            print(">>> " + country + ", " + sub + ", " + city)
+        except:
+            try:
+                real_ip = socket.gethostbyname(src_ip)
+                country, sub, city = get_ip_location(real_ip)
+                print(">>> " + country + ", " + sub + ", " + city)
+            except:
+                print("Not found")
