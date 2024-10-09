2b51c2ee591fb7ac6462c4ad045009ee85c4a4d0
CodeZhifty <juulstausholmoscar@gmail.com>
2024-10-09 16:49:22 +0200

    Added whitelist functionallity to https_sniffer

--- https_sniffer.py
+++ https_sniffer.py
@@ -6,13 +6,18 @@ import os
 
 parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=200))
 parser.add_argument("-i", "--interface", help="Network interface", required=True)
+parser.add_argument("-w", "--whitelist", help="Path to txt file containing urls that should be sniffed", required=True)
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 args = parser.parse_args()
 
-interface = args.interface
+interface, whitelistpath = args.interface, args.whitelist
 
+whitelist = None
+if whitelistpath:
+    with open(whitelistpath, "r") as f:
+        whitelist = f.read().splitlines()
 
 tshark_path = None
 
@@ -44,25 +49,37 @@ process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.ST
 
 print("[!] Sniffing http & https packets.")
 
+def print_packet(columns):
+    src_ip = columns[0]
+    protocol = columns[1]
+    info = columns[2].split(" ")
+    http_host = columns[3]
+    https_host = columns[4]
+    epoch_time = columns[5]
+
+    if "HTTP" in protocol:
+        if "GET" in info:
+            url = http_host + info[1]
+            print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, Method: {info[0]}, URL: {url}")
+
+    if "TLS" in protocol:
+        if https_host:
+            print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, URL: {https_host}")
+
 try:
     for line in iter(process.stdout.readline, b""):
         columns = line.decode().strip().split("|")
         try:
-            src_ip = columns[0]
-            protocol = columns[1]
-            info = columns[2].split(" ")
             http_host = columns[3]
             https_host = columns[4]
-            epoch_time = columns[5]
 
-            if "HTTP" in protocol:
-                if "GET" in info:
-                    url = http_host + info[1]
-                    print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, Method: {info[0]}, URL: {url}")
-
-            if "TLS" in protocol:
-                if https_host:
-                    print(f"[+] Time: {epoch_time}, Source: {src_ip}, Protocol: {protocol}, URL: {https_host}")
+            if whitelist:
+                if len([True for url in whitelist for word in http_host.split(".")[1:-1] if word in url.split(".")[:-1]]) > 0:
+                    print_packet(columns)
+                if len([True for url in whitelist for word in https_host.split(".")[1:-1] if word in url.split(".")[:-1]]) > 0:
+                    print_packet(columns)
+            else:
+                print_packet(columns)
         except:
             continue
 except KeyboardInterrupt:
